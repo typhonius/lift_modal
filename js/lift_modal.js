aa0ae9f9ff7aa5d7f7f266ccc8c99c5190e53c00
Adam Malone <adam@adammalone.net>
2016-09-08 00:23:28 +1000

    Further enhancements to allow this module to pick up details on first page load

--- js/lift_modal.js
+++ js/lift_modal.js
@@ -6,46 +6,58 @@
   };
 
   function createModal() {
-    $("body").append('<div id="lift-modal-container"><div class="lift-modal"><div class="lift-modal-title"></div><div class="lift-modal-image"></div><div class="lift-modal-email"></div><div class="lift-modal-id"></div><div class="lift-modal-engagement"></div><div class="lift-modal-first"></div><div class="lift-modal-last"></div><div class="lift-modal-persona"></div></div></div>');
-    if (Drupal.settings.lift_modal.email) {
-      $(".lift-modal-title").html( "<p>Identified user</p>" );
-    } else {
-      $(".lift-modal-title").html( "<p>Unknown user</p>" );
+    // Wait until Lift cookie has been created.
+    if(typeof $.cookie('tc_ptid') === 'undefined'){
+      setTimeout(createModal, 10);
+      return;
     }
-    if (Drupal.settings.lift_modal.image) {
+
+    $("body").append('<div id="lift-modal-container"><div class="lift-modal"><div class="lift-modal-title"></div><div class="lift-modal-image"></div><div class="lift-modal-email"></div><div class="lift-modal-id"></div><div class="lift-modal-engagement"></div><div class="lift-modal-first"></div><div class="lift-modal-last"></div><div class="lift-modal-persona"></div></div></div>');
+    $(".lift-modal-id").html("ID: " + $.cookie('tc_ptid'));
+    $(".lift-modal-engagement").html("Engagement Score: " + Drupal.settings.lift_modal.engagement);
+
+    // if (name in Drupal.settings.lift_modal.email) {
+    //   $(".lift-modal-title").html(Drupal.settings.lift_modal.name);
+    // } else {
+    //   $(".lift-modal-title").html( "<p>Unknown user</p>" );
+    // }
+    if ("image" in Drupal.settings.lift_modal) {
       $(".lift-modal-image").html('<img src="' + Drupal.settings.lift_modal.image + '">');
     }
-    if (Drupal.settings.lift_modal.email) {
+    if ("email" in Drupal.settings.lift_modal) {
       $(".lift-modal-email").html("Email: " + Drupal.settings.lift_modal.email);
     }
-    if (Drupal.settings.lift_modal.name) {
+    if ("name" in Drupal.settings.lift_modal) {
       $(".lift-modal-id").html("Name: " + Drupal.settings.lift_modal.name);
     }
-    if (Drupal.settings.lift_modal.id) {
-      $(".lift-modal-id").html("ID: " + Drupal.settings.lift_modal.id);
-    }
-    if (Drupal.settings.lift_modal.engagement > 0) {
-      $(".lift-modal-engagement").html("Engagement Score: " + Drupal.settings.lift_modal.engagement);
-    }
-    if (Drupal.settings.lift_modal.first) {
+
+
+    if ("first" in Drupal.settings.lift_modal) {
       var firstDate = new Date(Drupal.settings.lift_modal.first);
-      var firstHour = firstDate.getHours();
-      var firstMinute = firstDate.getMinutes();
-      var firstDay = firstDate.getDay();
-      var firstMonth = firstDate.getMonth();
-      var firstYear = firstDate.getFullYear();
-      $(".lift-modal-first").html("First visit: " + firstDay + "/" + firstMonth + "/" + firstYear + " " + firstHour + ":" + firstMinute);
+    } else {
+      var firstDate = new Date.getTime();
     }
-    if (Drupal.settings.lift_modal.last) {
+
+     var firstHour = firstDate.getHours();
+     var firstMinute = firstDate.getMinutes();
+     var firstDay = firstDate.getDay();
+     var firstMonth = firstDate.getMonth();
+     var firstYear = firstDate.getFullYear();
+     $(".lift-modal-first").html("First visit: " + firstDay + "/" + firstMonth + "/" + firstYear + " " + firstHour + ":" + firstMinute);
+
+    if ("last" in Drupal.settings.lift_modal) {
       var lastDate = new Date(Drupal.settings.lift_modal.last);
-      var lastHour = lastDate.getHours();
-      var lastMinute = lastDate.getMinutes();
-      var lastDay = lastDate.getDay();
-      var lastMonth = lastDate.getMonth();
-      var lastYear = lastDate.getFullYear();
-      $(".lift-modal-last").html("Latest visit: " + lastDay + "/" + lastMonth + "/" + lastYear + " " + lastHour + ":" + lastMinute);
+      } else {
+      var lastDate = new Date.getTime();
     }
-    if (Drupal.settings.lift_modal.persona) {
+    var lastHour = lastDate.getHours();
+    var lastMinute = lastDate.getMinutes();
+    var lastDay = lastDate.getDay();
+    var lastMonth = lastDate.getMonth();
+    var lastYear = lastDate.getFullYear();
+    $(".lift-modal-last").html("Latest visit: " + lastDay + "/" + lastMonth + "/" + lastYear + " " + lastHour + ":" + lastMinute);
+
+    if ("persona" in Drupal.settings.lift_modal) {
       $(".lift-modal-persona").html("Persona: " + Drupal.settings.lift_modal.persona);
     }
   };
