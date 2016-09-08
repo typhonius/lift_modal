3cb62c40563d1b8c7517ceffed99708411f5c7a3
Adam Malone <adam@adammalone.net>
2016-09-08 12:55:11 +1000

    Adds show/hide. Fixes some demo stuff

--- js/lift_modal.js
+++ js/lift_modal.js
@@ -5,22 +5,51 @@
     },
   };
 
+  function showModal() {
+    $(".lift-modal").removeClass('hide');
+    $(".lift-modal-title").html("Acquia Lift Debug (displayed)");
+    $.cookie('lift_modal_showhide', 'show');
+    return;
+  }
+
+  function hideModal() {
+    $(".lift-modal").addClass('hide');
+    $(".lift-modal-title").html("Acquia Lift Debug (hidden)");
+    $.cookie('lift_modal_showhide', 'hide');
+    return;
+  }
+
   function createModal() {
+
     // Wait until Lift cookie has been created.
+    // TODO do we need to make this null?
     if(typeof $.cookie('tc_ptid') === 'undefined'){
       setTimeout(createModal, 10);
       return;
     }
 
-    $("body").append('<div id="lift-modal-container"><div class="lift-modal"><div class="lift-modal-title"></div><div class="lift-modal-image"></div><div class="lift-modal-email"></div><div class="lift-modal-id"></div><div class="lift-modal-engagement"></div><div class="lift-modal-first"></div><div class="lift-modal-last"></div><div class="lift-modal-persona"></div></div></div>');
+    $("body").append('<div id="lift-modal-container"><div class="lift-modal-title">Acquia Lift Debug</div><div class="lift-modal"><div class="lift-modal-name"></div><div class="lift-modal-image"></div><div class="lift-modal-email"></div><div class="lift-modal-id"></div><div class="lift-modal-engagement"></div><div class="lift-modal-first"></div><div class="lift-modal-last"></div><div class="lift-modal-persona"></div></div></div>');
+    if(typeof $.cookie('lift_modal_showhide') === 'object' || $.cookie('lift_modal_showhide') === 'show'){
+      showModal();
+    }
+    else {
+      hideModal();
+    }
+
+    $(".lift-modal-title").click(function() {
+      if ($(".lift-modal").hasClass("hide")) {
+        showModal();
+      } else {
+        hideModal();
+      }
+    });
+
     $(".lift-modal-id").html("ID: " + $.cookie('tc_ptid'));
     $(".lift-modal-engagement").html("Engagement Score: " + Drupal.settings.lift_modal.engagement);
 
-    // if (name in Drupal.settings.lift_modal.email) {
-    //   $(".lift-modal-title").html(Drupal.settings.lift_modal.name);
-    // } else {
-    //   $(".lift-modal-title").html( "<p>Unknown user</p>" );
-    // }
+    if ("name" in Drupal.settings.lift_modal) {
+      $(".lift-modal-name").html(Drupal.settings.lift_modal.name);
+    }
     if ("image" in Drupal.settings.lift_modal) {
       $(".lift-modal-image").html('<img src="' + Drupal.settings.lift_modal.image + '">');
     }
@@ -57,7 +86,7 @@
     var lastYear = lastDate.getFullYear();
     $(".lift-modal-last").html("Latest visit: " + lastDay + "/" + lastMonth + "/" + lastYear + " " + lastHour + ":" + lastMinute);
 
-    if ("persona" in Drupal.settings.lift_modal) {
+    if ("persona" in Drupal.settings.lift_modal && Drupal.settings.lift_modal.persona !== null) {
       $(".lift-modal-persona").html("Persona: " + Drupal.settings.lift_modal.persona);
     }
   };
